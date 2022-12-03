8cc754ff7620605c087161d124364d4e013ac543
Ihor-Samburenko <115349844+Ihor-Samburenko@users.noreply.github.com>
2022-12-03 11:10:25 +0000

    Deploying to gh-pages from @ Ihor-Samburenko/master-code@e14b953b2ad1c4147f77dc620f05b77552ae4378 🚀

--- index.ee87385a.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-modal-apartments-open]"),closeModalBtn:document.querySelector("[data-modal-apartments-close]"),modal:document.querySelector("[data-modal-apartments]")};function t(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",t),e.closeModalBtn.addEventListener("click",t)}();
-//# sourceMappingURL=index.ee87385a.js.map
