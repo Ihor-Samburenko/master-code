cc9a1684b86fed6546d2b3d45e5ddaf81249df16
Ihor-Samburenko <samburenko.igor@gmail.com>
2022-12-04 16:23:20 +0200

    add animation styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './js/scroll';
+import './js/modal-contact';
 import './js/mobile-menu';
 import './js/modal-apartments';
 import './js/modal-apartments-download';
-import './js/modal-contact';
 import './js/gallary';
